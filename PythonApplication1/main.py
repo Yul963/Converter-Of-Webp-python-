7a4125a5c86e6fe2116995576254b8cc6e6ec28c
이율규 <dbfrb9786@gmail.com>
2022-04-02 00:06:22 +0900

    gui수정

--- PythonApplication1/main.py
+++ PythonApplication1/main.py
@@ -11,50 +11,28 @@ from mainwindow import Ui_MainWindow
 import timeit
 from queue import Queue
 
-class fileQue():
-    def __init__(self):
-        self.que = Queue()
-    def AddQue(self,path):
-        i = 0
-        if os.path.isfile(path):
-            name, ext = os.path.splitext(path)
-            file=os.path.basename(path)
-            if ext=='.jpg' or ext=='.png' or ext=='.gif':
-                self.que.put(path)
-            elif ext==".zip":
-                if 1==zip.checkzip(path):
-                    self.que.put(path)
-            else:
-                print("NOT CONVERTABLE")
-        elif os.path.isdir(path):
-            for (path_file, directory, files) in os.walk(path):
-                    for file in files:
-                        path_name, ext = os.path.splitext(file)
-                        if ext=='.jpg' or ext=='.png' or ext=='.gif':
-                            self.que.put(path_file+'/'+file)
-                            i = i + 1
-                        elif ext==".zip":
-                            if 1==zip.checkzip(path_file+'/'+file):
-                                self.que.put(path_file+'/'+file)
-                                i = i + 1
-                    if i==0:
-                        print("NO CONVERTABLE FILES IN "+path)
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.setWindowIcon(QIcon('ic_baseline_double_arrow_24.xml'))
         self.setAcceptDrops(True)
-        self.fileque = fileQue()
-        self.worker = Worker(self.proc,self.fileque.que)
+        self.que = Queue()
+        self.worker = Worker(self.append_log,self.que,self.changeStatus)
         self.worker.start()
-    def proc(self, count): 
+        self.statusBar().showMessage('Ready')
+    def proc(self, count):
         print("%fseconds." % (count))
-
+    def changeStatus(self, status):
+        self.statusBar().showMessage(status)
+    def append_filetext(self,text):
+        self.textBrowser_2.append(text)
+    def append_log(self,text):
+        self.textBrowser.append(text)
     def closeEvent(self, e):
         self.hide()
         self.worker.stop()
-
     def button_clicked(self):
         self.textBrowser.append()
     def dragEnterEvent(self, e):
@@ -71,33 +49,69 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             e.setDropAction(Qt.CopyAction)
             e.accept()
             for url in e.mimeData().urls():
-                self.fileque.AddQue(url.toLocalFile())
+                self.AddQue(url.toLocalFile())
         else: 
             e.ignore()
+    def AddQue(self,path):
+        i = 0
+        if os.path.isfile(path):
+            name, ext = os.path.splitext(path)
+            filename=os.path.basename(path)
+            if ext=='.jpg' or ext=='.png' or ext=='.gif':
+                self.que.put(path)
+                self.textBrowser_2.append(filename)
+            elif ext==".zip":
+                if 1==zip.checkzip(path):
+                    self.que.put(path)
+                    self.textBrowser_2.append(filename)
+            else:
+                self.textBrowser.append("NOT CONVERTABLE")
+        elif os.path.isdir(path):
+            for (path_file, directory, files) in os.walk(path):
+                    for file in files:
+                        path_name, ext = os.path.splitext(file)
+                        if ext=='.jpg' or ext=='.png' or ext=='.gif':
+                            self.que.put(path_file+'/'+file)
+                            self.textBrowser_2.append(file)
+                            i = i + 1
+                        elif ext==".zip":
+                            if 1==zip.checkzip(path_file+'/'+file):
+                                self.que.put(path_file+'/'+file)
+                                self.textBrowser_2.append(file)
+                                i = i + 1
+                    if i==0:
+                        self.textBrowser.append("NO CONVERTABLE FILES IN "+path)
 
 class Worker(QThread): 
-    def __init__(self, proc, que): 
+    def __init__(self, log, que, status): 
         super().__init__()
-        self.proc = proc
-        self.que = que 
+        self.log = log
+        self.que = que
+        self.status = status
     def run(self): 
-        i=0
+        i=True
         while(True):
-            if (self.que.qsize!=0):
-                if (i==0):
-                    start_time = timeit.default_timer() # 시작 시간 체크
-                    i+=1
-                webp.convert(self.que.get())
-            if (i==1):
-                terminate_time = timeit.default_timer() # 종료 시간 체크
-                count=terminate_time - start_time
-                self.emit(SIGNAL(self.proc(count)))
-                i-=1
+            file=self.que.get()
+            if (i):
+                start_time = timeit.default_timer() # 시작 시간 체크
+                self.emit(SIGNAL(self.status("Working")))
+                i=False
+            webp.convert(file)
+            filename=os.path.basename(file)
+            if (self.que.empty()):
+                if not (i):
+                    terminate_time = timeit.default_timer() # 종료 시간 체크
+                    count = terminate_time - start_time
+                    self.emit(SIGNAL(self.log(str(count)+'seconds.')))
+                    self.emit(SIGNAL(self.status("Ready")))
+                    i=True
+
     def stop(self):
         self.working = False
         self.quit()
         self.wait(1000)
 
+ui2py.py()
 app = QApplication()
 win = MainWindow()
 win.show()
--- PythonApplication1/mainwindow.py
+++ PythonApplication1/mainwindow.py
@@ -16,15 +16,14 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QGraphicsView, QMainWindow, QMenuBar,
-    QSizePolicy, QStatusBar, QTextBrowser, QVBoxLayout,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMenuBar, QSizePolicy,
+    QStatusBar, QTextBrowser, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(577, 473)
+        MainWindow.resize(510, 438)
         self.actionNew = QAction(MainWindow)
         self.actionNew.setObjectName(u"actionNew")
         self.centralwidget = QWidget(MainWindow)
@@ -34,21 +33,25 @@ class Ui_MainWindow(object):
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.textBrowser = QTextBrowser(self.centralwidget)
         self.textBrowser.setObjectName(u"textBrowser")
+        self.textBrowser.setLineWidth(1)
+        self.textBrowser.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
 
         self.verticalLayout.addWidget(self.textBrowser)
 
-        self.graphicsView = QGraphicsView(self.centralwidget)
-        self.graphicsView.setObjectName(u"graphicsView")
+        self.textBrowser_2 = QTextBrowser(self.centralwidget)
+        self.textBrowser_2.setObjectName(u"textBrowser_2")
 
-        self.verticalLayout.addWidget(self.graphicsView)
+        self.verticalLayout.addWidget(self.textBrowser_2)
 
+        self.verticalLayout.setStretch(0, 4)
+        self.verticalLayout.setStretch(1, 2)
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
         self.menuBar = QMenuBar(MainWindow)
         self.menuBar.setObjectName(u"menuBar")
-        self.menuBar.setGeometry(QRect(0, 0, 577, 22))
+        self.menuBar.setGeometry(QRect(0, 0, 510, 22))
         MainWindow.setMenuBar(self.menuBar)
 
         self.retranslateUi(MainWindow)
@@ -57,7 +60,19 @@ class Ui_MainWindow(object):
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Converter Of Webp", None))
+#if QT_CONFIG(tooltip)
+        MainWindow.setToolTip(QCoreApplication.translate("MainWindow", u"COW", None))
+#endif // QT_CONFIG(tooltip)
         self.actionNew.setText(QCoreApplication.translate("MainWindow", u"New", None))
+#if QT_CONFIG(tooltip)
+        self.centralwidget.setToolTip("")
+#endif // QT_CONFIG(tooltip)
+#if QT_CONFIG(tooltip)
+        self.textBrowser.setToolTip(QCoreApplication.translate("MainWindow", u"Progress", None))
+#endif // QT_CONFIG(tooltip)
+#if QT_CONFIG(tooltip)
+        self.statusbar.setToolTip(QCoreApplication.translate("MainWindow", u"Status Bar", None))
+#endif // QT_CONFIG(tooltip)
     # retranslateUi
 
